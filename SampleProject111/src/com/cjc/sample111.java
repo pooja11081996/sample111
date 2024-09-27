6fd968d25a391ec93a849c14a55d6410ea1b0652
aaradhyan <Aaradhya.Nachankar@tapasi.com>
2024-09-27 17:39:15 +0530

    By11111

---
+++ SampleProject111/src/com/cjc/sample111.java
@@ -0,0 +1,12 @@
+package com.cjc;
+
+public class sample111 
+{
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+public void m1()
+{
+	System.out.println("m1 method");
+}
+}
